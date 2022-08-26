05ab8e02bc64bb271b02b8ff2223592d0a4fe74b
glegendr <glegendr@student.42.fr>
2022-08-26 17:54:19 +0200

    ex09

--- src/exo_09.rs
+++ src/exo_09.rs
@@ -6,7 +6,7 @@ pub enum Operator {
     Xor,
     Material,
     Equal,
-    Set(Vec<i32>)
+    Set((Vec<i32>, bool))
 }
 
 #[derive(Debug, Clone)]
@@ -15,6 +15,7 @@ pub struct BTree {
     pub c2: Option<Box<BTree>>,
     pub node: Operator
 }
+
 
 impl BTree {
     pub fn new(node: Operator) -> BTree {
@@ -25,6 +26,22 @@ impl BTree {
         }
     }
 
+    pub fn not(sub_tree: BTree) -> BTree {
+        BTree {
+            c1: None,
+            c2: Some(Box::new(sub_tree)),
+            node: Operator::Not
+        }
+    }
+
+    pub fn create(node: Operator, c1: BTree, c2: BTree) -> BTree {
+        BTree {
+            node,
+            c1: Some(Box::new(c1)),
+            c2: Some(Box::new(c2))
+        }
+    }
+
     pub fn insert_a(&mut self, sub_tree: BTree) {
         self.c1 = Some(Box::new(sub_tree));
     }
@@ -32,7 +49,110 @@ impl BTree {
     pub fn insert_b(&mut self, sub_tree: BTree) {
         self.c2 = Some(Box::new(sub_tree));
     }
+
+    fn insert_not(&self) -> Self {
+        let mut new = BTree::not((*self).clone());
+        new.modify();
+        new
+    }
+
+    fn modify(&mut self) {
+        match (&self.node, &mut  self.c1, &mut self.c2) {
+            (Operator::Material, Some(c1), Some(c2)) => {
+                c1.modify();
+                c2.modify();
+                *self = BTree::create(Operator::Or, c1.insert_not(), (**c2).clone())
+            }
+            (Operator::Xor, Some(c1), Some(c2)) => {
+                // AB!&A!B&|
+                let mut ret = BTree::new(Operator::Or);
+                c1.modify();
+                c2.modify();
+                //AB!& C1
+                ret.insert_a(BTree::create(Operator::And, (**c1).clone(), c2.insert_not()));
+                //A!B& C2
+                ret.insert_b(BTree::create(Operator::And, c1.insert_not(), (**c2).clone()));
+                *self = ret;
+            }
+            (Operator::Equal, Some(c1), Some(c2)) => {
+                let mut ret = BTree::new(Operator::Or);
+                c1.modify();
+                c2.modify();
+                // let a = modify_tree(&c1);
+                // let b = modify_tree(&c2);
+                //AB& C1
+                let mut new_c1 = BTree::new(Operator::And);
+                new_c1.insert_a((**c1).clone());
+                new_c1.insert_b((**c2).clone());
+                ret.insert_a(new_c1);
     
+                //A!B!& C2
+                let mut new_c2 = BTree::new(Operator::And);
+                new_c2.insert_a(c1.insert_not());
+                new_c2.insert_b(c2.insert_not());
+                ret.insert_b(new_c2);
+                *self = ret;
+            }
+            (Operator::Not, None, Some(c2)) => match &c2.node {
+                Operator::Set(_) => {},
+                _ => {
+                    c2.modify();
+                    match &c2.node {
+                        Operator::And => *self = BTree::create(
+                            Operator::Or,
+                            c2.c1.as_ref().unwrap().insert_not(),
+                            c2.c2.as_ref().unwrap().insert_not(),
+                        ),
+                        Operator::Or => *self = BTree::create(
+                            Operator::And,
+                            c2.c1.as_ref().unwrap().insert_not(),
+                            c2.c2.as_ref().unwrap().insert_not(),
+                        ),
+                        Operator::Not => *self = (**c2.c2.as_ref().unwrap()).clone(),
+                        _ => {},
+                    }
+                }
+            },
+            (Operator::Set(_), None, None) => {},
+            (_, Some(c1), Some(c2)) => {
+                let mut ret = BTree::new(self.node.clone());
+                c1.modify();
+                c2.modify();
+                ret.insert_a((**c1).clone());
+                ret.insert_b((**c2).clone());
+                *self = ret
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn from_str(formula: &mut String, sets: &Vec<Vec<i32>>) -> Result<Self, String> {
+        if let Some(last_c) = formula.pop() {
+            let mut ret = match last_c {
+                '&' => BTree::new(Operator::And),
+                '|' => BTree::new(Operator::Or),
+                '^' => BTree::new(Operator::Xor),
+                '>' => BTree::new(Operator::Material),
+                '=' => BTree::new(Operator::Equal),
+                '!' => BTree::new(Operator::Not),
+                'A'..='Z' => {
+                    match sets.get((last_c as u8 - 65) as usize) {
+                        Some(set) => return Ok(BTree::new(Operator::Set((set.clone(), true)))),
+                        None => return Err(format!("no existing set for {last_c}"))
+                    }
+                    
+                },
+                c => return Err(format!("unexpected variable {c}"))
+            };
+            ret.insert_b(BTree::from_str(formula, sets)?);
+            if last_c != '!' {
+                ret.insert_a(BTree::from_str(formula, sets)?);
+            }
+            return Ok(ret)
+        }
+        Err(String::from("Error while parsing formula"))
+    }
+
     pub fn to_string(&self) -> String {
         match (&self.node, &self.c1, &self.c2) {
             (Operator::And, Some(c1), Some(c2)) => format!("{}{}&", c1.to_string(), c2.to_string()),
@@ -42,40 +162,111 @@ impl BTree {
             (Operator::Material, Some(c1), Some(c2)) => format!("{}{}>", c1.to_string(), c2.to_string()),
             (Operator::Not, Some(c1), None) => format!("{}!", c1.to_string()),
             (Operator::Not, None, Some(c2)) => format!("{}!", c2.to_string()),
-            (Operator::Set(s), None, None) => format!("{s:?}"),
+            (Operator::Set((s, b)), _, _) => format!("{s:?}{}", if *b {""} else {"!"}),
             _ => format!("{self:?}")
         }
     }
 
-    fn from_str(mut formula: String) -> Result<Self, String> {
-        if let Some(last_c) = formula.pop() {
-            let mut ret = match last_c {
-                '&' => BTree::new(Operator::And),
-                '|' => BTree::new(Operator::Or),
-                '^' => BTree::new(Operator::Xor),
-                '>' => BTree::new(Operator::Material),
-                '=' => BTree::new(Operator::Equal),
-                '!' => BTree::new(Operator::Not),
-                'A'..='Z' => BTree::new(Operator::Set(vec![])),
-                _ => Err(format!)
-            };
-            ret.insert_b(BTree::from_str(formula)?);
-            if last_c != '!' {
-                ret.insert_a(BTree::from_str(formula)?);
-            }
-            return Ok(ret)
+
+
+    fn eval(&self) -> Result<(Vec<i32>, bool), String> {
+        match (&self.node, &self.c1, &self.c2) {
+            (Operator::And, Some(c1), Some(c2)) => {
+                let ret = intersect(c1.eval()?, c2.eval()?);
+                Ok(ret)
+            },
+            (Operator::Or, Some(c1), Some(c2)) => {
+                let (mut a, a_b) = c1.eval()?;
+                let (mut b, b_b) = c2.eval()?;
+                match (a_b, b_b) {
+                    (true, true) => {
+                        a.append(&mut b);
+                        a.sort_unstable();
+                        a.dedup();
+                        Ok((a, true))
+                    }
+                    (true, false) => {
+                        let mut ret = Vec::new();
+                        for child in b {
+                            if !a.contains(&child) {
+                                ret.push(child)
+                            }    
+                        }
+                        Ok((ret, false))
+                    }
+                    (false, true) => {
+                        let mut ret = Vec::new();
+                        for child in a {
+                            if !b.contains(&child) {
+                                ret.push(child)
+                            }    
+                        }
+                        Ok((ret, false))
+                    }
+                    (false, false) => {
+                        let (ret, _) = intersect((a, true), (b, true));
+                        Ok((ret, false))
+                    }
+                }
+            },
+            (Operator::Not, Some(c), None) | (Operator::Not, None, Some(c))=> {
+                let (set, b) = c.eval()?;
+                Ok((set, !b))
+            },
+            (Operator::Set(set), None, None) => Ok(set.clone()),
+            _ => return Err(String::from("Error while calculating formula")),
         }
-        Err(String::from("Error while parsing formula"))
     }
 }
 
-
+fn intersect((mut a, bool_a): (Vec<i32>, bool), (mut b, bool_b): (Vec<i32>, bool)) -> (Vec<i32>, bool) {
+    let mut ret = Vec::new();
+    match (bool_a, bool_b) {
+        (true, true) => {
+            for child in a {
+                if b.contains(&child) {
+                    ret.push(child);
+                }
+            }
+            (ret, true)
+        }
+        (true, false) => {
+            for child in a {
+                if !b.contains(&child) {
+                    ret.push(child);
+                }
+            }
+            (ret, true)
+        }
+        (false, true) => {
+            for child in b {
+                if !a.contains(&child) {
+                    ret.push(child);
+                }
+            }
+            (ret, true)
+        }
+        (false, false) => {
+            a.append(&mut b);
+            (a, false)
+        }
+    }
+}
 
 #[allow(dead_code)]
 fn eval_set(formula: &str, sets: Vec<Vec<i32>>) -> Vec<i32> {
-    match BTree::from_str(formula.to_owned()) {
-        Ok(tree) => {
-            println!("{}", tree.to_string());
+    match BTree::from_str(&mut formula.to_owned(), &sets) {
+        Ok(mut tree) => {
+            tree.modify();
+            match tree.eval() {
+                Ok((res, b)) => {
+                    return match b {
+                        true => res,
+                        false => Vec::new()
+                    }
+                },
+                Err(e) => println!("{e}")
+            }
         },
         Err(e) => {
             println!("{e}");
@@ -90,9 +281,9 @@ mod eval_set {
     use super::*;
     #[test]
     fn eval_set_test() {
-        eval_set("AB&", vec![vec![1,2], vec![2,3]]);
-        eval_set("A!", vec![vec![1,2], vec![2,3]]);
-        eval_set("''&", vec![vec![1,2], vec![2,3]]);
-        assert_eq!(1,2);
+        assert_eq!(eval_set("A!", vec![vec![1,2]]), vec![]);
+        assert_eq!(eval_set("AB&", vec![vec![1,2], vec![2,3]]), vec![2]);
+        assert_eq!(eval_set("AB^", vec![vec![1,2], vec![2,3]]), vec![1,3]);
+        assert_eq!(eval_set("AB|", vec![vec![1,2], vec![2,3]]), vec![1,2,3]);
     }
 }
